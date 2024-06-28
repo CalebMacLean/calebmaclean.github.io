67c72d7c7148b4cdfb215017066eab06fc8785ef
caleb <CMaclean98@gmail.com>
2024-06-28 13:45:46 -0700

    Created db.js to dynamically select proper database and connect to it, Initiated npm, Updated .gitignore with backend/node_modules

---
+++ backend/db.js
@@ -0,0 +1,31 @@
+// Enable strict mode to help catch common coding mistakes
+"use strict";
+
+/** Database setup for Pomodoro App */
+// Imports
+const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config")
+
+// declare mutable database variable
+let db;
+
+// modify db to use either the db or testing db depending on enviornment
+if ( process.env.NODE_ENV === 'production') {
+    db = new Client({
+        connectionString: getDatabaseUri(),
+        ssl: {
+            rejectUnauthorized: false;
+        }
+    })
+}
+else {
+    db = new Client({
+        connectionString: getDatabaseUri(),
+    })
+}
+
+// connect application to database
+db.connect();
+
+// Exports
+module.exports = db;
