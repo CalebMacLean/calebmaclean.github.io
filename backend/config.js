e40aa2331b01a9fa3d2b2cefc0ebf365ba9e0442
caleb <CMaclean98@gmail.com>
2024-07-05 12:51:47 -0700

    Fixed connnection bug where getDatabaseUri was returning undefined uri variables by: Making the default parameters equal to the process.env variables, Changing the user's postgres password, Updated expectations in config.test.js to reflect new password, Updated the test script in package.json to set the NODE_ENV to test.

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,15 @@
 /** Shared Configuration for backend of application, can be required in many places */
 // Imports
 require("dotenv").config();
+
+// console.log("Process Env Object: ", process.env);
+
+// Access SECRET_KEY for export
 const SECRET_KEY = process.env.SECRET_KEY || "shadow-dev";
-const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 /** Database Uri Function
  * 
@@ -13,10 +20,25 @@ const PORT = process.env.PORT || 3001;
  * 
  * Parameters: -none
  */
-function getDatabaseUri() {
-    return ( process.env.NODE_ENV === "test" )
-        ? "pomodoro_test"
-        : process.env.DATABASE_URL || "pomodoro";
+function getDatabaseUri(user = DB_USER, password = DB_PASSWORD, host = DB_HOST, port = DB_PORT) {
+    // log parameters vals
+    console.log('user: ', user);
+    console.log('password: ', password);
+    console.log('host: ', host);
+    console.log('port: ', port);
+
+    // env vars
+    // const u = process.env.DB_USER;
+    // const p = process.env.DB_PASSWORD;
+    // const h = process.env.DB_HOST
+    // const port = process.env.DB_PORT;
+
+    // database name depends on app's environment
+    const name = process.env.NODE_ENV === "test" ? "pomodoro_test" : "pomodoro";
+
+    console.log(`postgres://${user}:${password}@${host}:${port}/${name}`);
+
+    return `postgres://${user}:${password}@${host}:${port}/${name}`;
 };
 
 // Speed up bcrypt during tests
@@ -26,7 +48,7 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 console.log("-------------------------------------------");
 console.log("Pomodoro Config:");
 console.log("SECRET_KEY: ", SECRET_KEY);
-console.log("PORT: ", PORT.toString());
+// console.log("PORT: ", PORT.toString());
 console.log("BCRYPT_WORK_FACTOR: ", BCRYPT_WORK_FACTOR);
 console.log("Database: ", getDatabaseUri());
 console.log("-------------------------------------------");
@@ -34,7 +56,6 @@ console.log("-------------------------------------------");
 // Exports
 module.exports = {
     SECRET_KEY,
-    PORT,
     BCRYPT_WORK_FACTOR,
     getDatabaseUri
 }
--- backend/config.test.js
+++ backend/config.test.js
@@ -1,33 +1,33 @@
 // Enable strict mode to catch common mistakes
 "use strict";
 
-describe("config can come from env", function() {
-    test("works", function() {
-        // Set env vars
-        process.env.SECRET_KEY = "abc";
-        process.env.PORT = "5000";
-        process.env.DATABASE_URL = "other";
-        process.env.NODE_ENV = "other";
+// Imports
+const config = require('./config');
 
-        // Import config settings from config.js
-        const config = require("./config");
-        // test that settings are set to previously created env vars
-        expect(config.SECRET_KEY).toEqual("abc");
-        expect(config.PORT).toEqual("5000");
-        expect(config.getDatabaseUri()).toEqual("other");
-        expect(config.BCRYPT_WORK_FACTOR).toEqual(12);
+describe("config can come from env", function () {
+    test('should contain SECRET_KEY', function () {
+        expect(config).toHaveProperty('SECRET_KEY');
+        expect(typeof config.SECRET_KEY).toBe('string');
+    })
 
-        // clean up env vars
-        delete process.env.SECRET_KEY;
-        delete process.env.PORT;
-        delete process.env.BCRYPT_WORK_FACTOR;
-        delete process.env.DATABASE_URL;
+    test('should contain BCRYPT_WORK_FACTOR', function () {
+        expect(config).toHaveProperty('BCRYPT_WORK_FACTOR');
+        expect(typeof config.BCRYPT_WORK_FACTOR).toBe('number');
+    })
 
-        // check that getDatabaseUri in non test env returns "pomodoro"
-        expect(config.getDatabaseUri()).toEqual("pomodoro");
+    test('getDatabaseUri should return a string', () => {
+        expect(typeof config.getDatabaseUri()).toBe('string');
+    })
 
-        // check that getDatabaseUri in test env returns "pomodoro_test"
-        process.env.NODE_ENV = "test";
-        expect(config.getDatabaseUri()).toEqual("pomodoro_test");
+    test('getDatabaseUri should return a test database uri', function () {
+        // set app env to test
+        process.env.NODE_ENV = 'test';
+        expect(config.getDatabaseUri()).toBe('postgres://caleb:Maclea708@localhost:5432/pomodoro_test');
+    })
+
+    test('getDatabaseUri should return main database uri in non-test enviornments', function () {
+        // set app env to not test
+        process.env.NODE_ENV = 'production';
+        expect(config.getDatabaseUri()).toBe('postgres://caleb:Maclea708@localhost:5432/pomodoro');
     })
 })
