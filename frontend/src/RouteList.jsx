ab9aaabc6bc6a93e0f8196c02dc7eb28525d2968
caleb <CMaclean98@gmail.com>
2024-07-27 14:26:05 -0700

    Revert "going to delete and restart my vite application"
    This reverts commit 3954a751a8468e0572eef3c2e5c404e2b92b15ec.

--- frontend/src/RouteList.jsx
+++ frontend/src/RouteList.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
-// import Signup from './Signup';
-// import Logout from './Logout';
-// import Profile from './Profile';
+import Signup from './Signup';
+import Logout from './Logout';
+import Profile from './Profile';
 import NotFound from './NotFound';
 
 /** RouteList Component
@@ -20,10 +20,10 @@ const RouteList = ({ logout, login, username }) => {
     return (
         <Routes>
             <Route path="/" element={<Home username={ username } />} />
-            {/* <Route path="/login" element={<Login login={ login } />} /> */}
-            {/* <Route path="/signup" element={<Signup login={ login } />} /> */}
-            {/* <Route path="/profile" element={<Profile username={ username } />} /> */}
-            {/* <Route path="/logout" element={<Logout logout={ logout }/>} /> */}
+            <Route path="/login" element={<Login login={ login } />} />
+            <Route path="/signup" element={<Signup login={ login } />} />
+            <Route path="/profile" element={<Profile username={ username } />} />
+            <Route path="/logout" element={<Logout logout={ logout }/>} />
             <Route path="*" element={<NotFound />} />   
         </Routes>
     )
