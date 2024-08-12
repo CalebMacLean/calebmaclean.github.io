84e6cb42d29d57a84200955052e37c09f53bfb67
caleb <CMaclean98@gmail.com>
2024-08-12 13:17:19 -0700

    Regressing to the basic strategies for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@
 
 /** Server start up logic */
 // Imports
-const express = require('express');
-const app = require("./app");
+// const express = require('express');
+// const app = require("./app");
 const { PORT } = require("./config");
-const path = require('path');
+// const path = require('path');
 
-app.use(express.static(path.join('..', 'frontend', 'dist')));
+// app.use(express.static(path.join('..', 'frontend', 'dist')));
 
-app.get('*', (req, res, next) => {
-    res.sendFile(path.join('..', 'frontend', 'dist', 'index.html'));
-});
+// app.get('*', (req, res, next) => {
+//     res.sendFile(path.join('..', 'frontend', 'dist', 'index.html'));
+// });
 
 app.listen(PORT, function() {
     console.log(`Started on http://localhost:${PORT}`);
